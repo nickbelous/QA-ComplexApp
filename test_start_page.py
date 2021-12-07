198ea7b2e005b44e7e5a83dcf396d745c5cfd50a
nickbelous <nic.belous@gmail.com>
2021-12-07 13:27:27 +0200

    Homework 02.12.2021 - realize 5 testcases for our app

--- test_start_page.py
+++ test_start_page.py
@@ -112,3 +112,63 @@ class TestStartPage:
     def random_num_registration(self):
         """Generate random number"""
         return str(random.choice(range(111111111111, 999999999999)))
+
+    def test_tc1(self):
+        """TC1 - After click to text "Complex app for testing - QA" redirected to main page """
+        # Create driver
+        driver = webdriver.WebDriver(executable_path="./drivers/chromedriver.exe")
+        # Open start page
+        driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+        # Found the text
+        checked_text_tc1 = driver.find_element(by=By.XPATH, value=".//H4")
+        # Click on the link
+        checked_text_tc1.click()
+        sleep(2)
+        # Check link on the page
+        checked_text_tc1 = driver.find_element(by=By.XPATH, value=".//H4")
+        checked_text_tc1.is_displayed()
+
+    def test_tc2(self):
+        """Check text "Remember Writing?" is display in main page """
+        # Create driver
+        driver = webdriver.WebDriver(executable_path="./drivers/chromedriver.exe")
+        # Open start page
+        driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+        # Found the text
+        checked_text_tc2 = driver.find_element(by=By.XPATH, value=".//h1")
+        checked_text_tc2.is_displayed()
+
+    def test_tc3(self):
+        """Check text on "Sign up for OurApp"""
+        # Create driver
+        driver = webdriver.WebDriver(executable_path="./drivers/chromedriver.exe")
+        # Open start page
+        driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+        # Found the text
+        checked_text_tc3 = driver.find_element(by=By.XPATH, value=".//button[@type='submit']")
+        checked_text_tc3.is_displayed()
+
+    def test_tc4(self):
+        """Check text "Are you sick of short..." is display on the page"""
+        # Create driver
+        driver = webdriver.WebDriver(executable_path="./drivers/chromedriver.exe")
+        # Open start page
+        driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+        # Found the text
+        checked_text_tc4 = driver.find_element(by=By.XPATH, value=".//p[@class='lead text-muted']")
+        checked_text_tc4.is_displayed()
+
+    def test_tc5(self):
+        """Check after clicking on the text "Our App" redirected is happened"""
+        # Create driver
+        driver = webdriver.WebDriver(executable_path="./drivers/chromedriver.exe")
+        # Open start page
+        driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+        # Found the text
+        checked_text_tc5 = driver.find_element(by=By.XPATH, value=".//a[@class='text-muted']")
+        # Click on the link
+        checked_text_tc5.click()
+        sleep(2)
+        # Check link on the page
+        checked_text_tc5 = driver.find_element(by=By.XPATH, value=".//a[@class='text-muted']")
+        checked_text_tc5.is_displayed()
